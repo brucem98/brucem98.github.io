1f3ba16c8d71562cbd934f12299920a0044e67d0
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-09-12 13:08:32 +0100

    Add new project

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let eventTarget = document.querySelectorAll(".target_element");
+// let eventTarget = document.querySelectorAll(".target_element");
 
-let clickEvent = (event) => {
-  let parent = event.target.parentNode;
-  for (let i = 1; i < parent.children.length; i++) {
-    let child = parent.children[i];
-    child.style.display =
-      window.getComputedStyle(child).display === "none" ? "block" : "none";
-  }
-};
+// let clickEvent = (event) => {
+//   let parent = event.target.parentNode;
+//   for (let i = 1; i < parent.children.length; i++) {
+//     let child = parent.children[i];
+//     child.style.display =
+//       window.getComputedStyle(child).display === "none" ? "block" : "none";
+//   }
+// };
 
-eventTarget.forEach((item) => {
-  item.addEventListener("click", clickEvent);
-});
+// eventTarget.forEach((item) => {
+//   item.addEventListener("click", clickEvent);
+// });
 
 // // addEventListenerListener() syntax allows for multiple event handler functions
 // // eventTarget.addEventListener('click', eventHandlerFunction);
